af4b5afc6ee60eb0cfc0a735f1b3217cddcb88df
Zuzana Kaczmarczyková <kaczmarczykova@gmail.com>
2025-04-01 20:05:42 +0200

    rybka plave dokola

--- src/main/java/cz/czechitas/java/lekce01/RybkaApplication.java
+++ src/main/java/cz/czechitas/java/lekce01/RybkaApplication.java
@@ -36,5 +36,19 @@ public class RybkaApplication extends CzechitasGameApplication {
     rybka = new Rybka(100, 100);
 
     //TODO tady napište svůj kód
+
+
+    for (int i = 0; i <= 20; i++) {
+      rybka.plavatVpravo();
+    }
+    for (int i = 0; i <= 20; i++) {
+      rybka.plavatDolu();
+    }
+    for (int i = 0; i <= 20; i++) {
+      rybka.plavatVlevo();
+    }
+    for (int i = 0; i <= 20; i++) {
+      rybka.plavatNahoru();
+    }
   }
 }
